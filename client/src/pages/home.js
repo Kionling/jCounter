fbb075a4b9a9348211972d8bb6f6ae38c22e688d
Daniel Jauregui <djvdjv209@gmail.com>
2023-11-20 01:52:43 -0800

    Adding more functionality to the main page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,61 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../pages/home.css";
 import "../pages/home.css";
+
+
+
+    
 
 function Home() {
-  const [hiredCount, setHiredCount] = useState(0);
-  const [declinedCount, setDeclinedCount] = useState(0);
-  const [noAnswerCount, setNoAnswerCount] = useState(0);
-  const [applicationsSentCount, setApplicationsSentCount] = useState(0);
-  const handleResetClick = () => {
-    const confirmed = window.confirm(
-      "Are you sure you want to reset the counts?"
+    const [hiredCount, setHiredCount] = useState(0);
+    const [declinedCount, setDeclinedCount] = useState(0);
+    const [noAnswerCount, setNoAnswerCount] = useState(0);
+    const [applicationsSentCount, setApplicationsSentCount] = useState(0);
+
+    useEffect(() => {
+        // Retrieve the counts from localStorage when the component mounts
+        const storedHiredCount = localStorage.getItem("hiredCount");
+        const storedDeclinedCount = localStorage.getItem("declinedCount");
+        const storedNoAnswerCount = localStorage.getItem("noAnswerCount");
+        const storedApplicationsSentCount = localStorage.getItem(
+            "applicationsSentCount"
+        );
+
+        // Update the state with the retrieved counts
+        setHiredCount(parseInt(storedHiredCount) || 0);
+        setDeclinedCount(parseInt(storedDeclinedCount) || 0);
+        setNoAnswerCount(parseInt(storedNoAnswerCount) || 0);
+        setApplicationsSentCount(parseInt(storedApplicationsSentCount) || 0);
+    }, []);
+
+    useEffect(() => {
+        // Store the counts in localStorage whenever they change
+        localStorage.setItem("hiredCount", hiredCount);
+        localStorage.setItem("declinedCount", declinedCount);
+        localStorage.setItem("noAnswerCount", noAnswerCount);
+        localStorage.setItem("applicationsSentCount", applicationsSentCount);
+    }, [hiredCount, declinedCount, noAnswerCount, applicationsSentCount]);
+
+    const handleResetClick = () => {
+        const confirmed = window.confirm(
+            "Are you sure you want to reset the counts?"
+        );
+        if (confirmed) {
+            setHiredCount(0);
+            setDeclinedCount(0);
+            setNoAnswerCount(0);
+            setApplicationsSentCount(0);
+        }
+    };
+
+    const handleHiredClick = () => {
+        setHiredCount(hiredCount + 1);
+    };
+
+    const handleDeclinedClick = () => {
+        setDeclinedCount(declinedCount + 1);
+    };
+
+    const handleNoAnswerClick = () => {
+        setNoAnswerCount(noAnswerCount + 1);
+    };
+
+    const handleApplicationsSentClick = () => {
+        setApplicationsSentCount(applicationsSentCount + 1);
+    };
+
+    const totalCount = hiredCount + declinedCount + noAnswerCount;
+
+    return (
+        <div className="container ">
+            <div>
+                <h2>Job Counter</h2>
+                <p>Hired: {hiredCount}</p>
+                <p>Declined: {declinedCount}</p>
+                <p>No Answer: {noAnswerCount}</p>
+                <p>Applications Sent: {applicationsSentCount}</p>
+                <p>Total: {totalCount}</p>
+            </div>
+            <div>
+                <button onClick={handleHiredClick}>Hired</button>
+                <button onClick={handleDeclinedClick}>Declined</button>
+                <button onClick={handleNoAnswerClick}>No Answer</button>
+                <button onClick={handleApplicationsSentClick}>Applications Sent</button>
+                <button onClick={handleResetClick}>Reset</button>
+            </div>
+        </div>
     );
-    if (confirmed) {
-      setHiredCount(0);
-      setDeclinedCount(0);
-      setNoAnswerCount(0);
-      setApplicationsSentCount(0);
-    }
-  };
-
-  const handleHiredClick = () => {
-    setHiredCount(hiredCount + 1);
-  };
-
-  const handleDeclinedClick = () => {
-    setDeclinedCount(declinedCount + 1);
-  };
-
-  const handleNoAnswerClick = () => {
-    setNoAnswerCount(noAnswerCount + 1);
-  };
-
-  const handleApplicationsSentClick = () => {
-    setApplicationsSentCount(applicationsSentCount + 1);
-  };
-
-  const totalCount = hiredCount + declinedCount + noAnswerCount;
-
-  return (
-    <div className="container center">
-      <div>
-        <h2>Job Counter</h2>
-        <p>Hired: {hiredCount}</p>
-        <p>Declined: {declinedCount}</p>
-        <p>No Answer: {noAnswerCount}</p>
-        <p>Applications Sent: {applicationsSentCount}</p>
-        <p>Total: {totalCount}</p>
-      </div>
-      <div>
-        <button onClick={handleHiredClick}>Hired</button>
-        <button onClick={handleDeclinedClick}>Declined</button>
-        <button onClick={handleNoAnswerClick}>No Answer</button>
-        <button onClick={handleApplicationsSentClick}>Applications Sent</button>
-        <button onClick={handleResetClick}>Reset</button>
-      </div>
-    </div>
-  );
 }
 
 export default Home;
